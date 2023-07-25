d67e5af7404f552fcb9a5d8742645925eca35201
dev-guilherme <guilhermepereiradv@gmail.com>
2023-07-24 23:26:33 -0300

    test: db connection

---
+++ tests/Unit/DbConnectionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use PDO;
+
+class DbConnectionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_if_connection_with_database_is_working(): void
+    {
+        $connection = DB::connection()->getPdo();
+
+        $this->assertTrue($connection instanceof PDO, 'Connection with database is not working');
+    }
+}
