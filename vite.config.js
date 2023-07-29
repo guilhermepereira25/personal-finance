81858d10519e72386fdb880abd01128d29224ac4
dev-guilherme <guilhermepereiradv@gmail.com>
2023-07-29 19:48:56 -0300

    chore: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: [
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
