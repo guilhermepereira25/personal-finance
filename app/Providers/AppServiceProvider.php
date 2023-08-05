1a9a98df66873b2f9e9363b116e43a7924b33d2a
dev-guilherme <guilhermepereiradv@gmail.com>
2023-08-05 00:17:26 -0300

    chore: app service provider boot dependecies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            Domain\Interfaces\UserFactory::class,
+            Factories\UserFactory::class
+        );
+
+        $this->app->bind(
+            Domain\Interfaces\UserRepository::class,
+            Repositories\UserRepository::class
+        );
+        
+        $this->app
+            ->when(HttpControllers\UserController::class)
+            ->needs(UseCases\CreateUser\CreateUserInputPort::class)
+            ->give(function ($app) {
+                return $app->make(UseCases\CreateUser\CreateUserInteractor::class, [
+                    'output' => $app->make(Presenters\CreateUserHttpPresenter::class),
+                ]);
+            });
     }
 
     /**
