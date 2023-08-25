bb1916412c25a98e2dea691feaf39940e2a911c9
dev-guilherme <guilhermepereiradv@gmail.com>
2023-08-24 23:40:53 -0300

    build: update dependecies

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
             input: [
                 'resources/js/app.js',
